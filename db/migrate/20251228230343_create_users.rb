475ee54784300cc54c73ee1985a9160328b3bc6f
Giang Nguyen <162690180+Giangsoda178@users.noreply.github.com>
2025-12-29 10:23:19 +1100

    Add User model, migration, and spec
    Introduces a basic User model with a corresponding migration to create the users table, including fields for authentication and roles. Also adds an initial RSpec test file for the User model.

---
+++ db/migrate/20251228230343_create_users.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users, id: :string, force: :cascade do |t|
+      t.string :first_name, null: false
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :two_fa_enabled
+      t.string :two_fa_secret
+      t.string :role, null: false, default: "user"
+
+      t.timestamps
+    end
+  end
+end
