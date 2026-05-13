f707c0f4a9a808dafc6b80f138073aad83d65f44
Giang Nguyen <162690180+Giangsoda178@users.noreply.github.com>
2026-05-13 22:52:35 +1000

    feat(calendar): parse month/day and return focused_date

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,6 +4,7 @@ class CalendarController < InertiaController
   def index
     month = parsed_month(params[:month])
     center = month || Date.today.beginning_of_month
+    focused_date = month.present? ? build_focused_date(center, params[:day]) : nil
     range_start = (center - 14).beginning_of_day
     range_end = (center.end_of_month + 14).end_of_day
 
@@ -14,7 +15,7 @@ class CalendarController < InertiaController
       reminders: reminders,
       occurrences: occurrences.map { |o| {reminder_id: o.reminder_id, date: o.date.iso8601} },
       today: Date.today.iso8601,
-      focused_date: month&.iso8601
+      focused_date: focused_date&.iso8601
     }
   end
 
@@ -26,5 +27,11 @@ class CalendarController < InertiaController
     Date.iso8601(raw_value).beginning_of_month
   rescue Date::Error
     nil
+  end
+
+  def build_focused_date(month, raw_day)
+    day = Integer(raw_day, exception: false)
+    normalized_day = day&.positive? ? day : 1
+    Date.new(month.year, month.month, [normalized_day, month.end_of_month.day].min)
   end
 end
