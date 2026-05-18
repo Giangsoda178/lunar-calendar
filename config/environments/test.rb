da834a435fa1b824a4f60d8a35bcb2575e1afc06
Leo Nguyen <162690180+Giangsoda178@users.noreply.github.com>
2026-05-19 09:45:34 +1000

    feat(jobs): deliver reminder alerts on a schedule

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,11 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {host: "example.com"}
 
+  # Set host for URL helpers used outside of controllers/mailers (e.g., jobs)
+  config.after_initialize do
+    Rails.application.routes.default_url_options[:host] = "example.com"
+  end
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
